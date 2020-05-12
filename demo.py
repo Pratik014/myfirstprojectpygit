364a440c06018c1e862500100e02144b47881f63
Pratik014 <65241340+Pratik014@users.noreply.github.com>
2020-05-12 21:37:37 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
+pos=-1
 
+def search(list,n):
+    i=0
+    while i<len(list):
+        if list[i]==n:
+            globals()['pos']=i
+            return True
+        i+=1
+    return False
+
+
+
+list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,23]
+n=23
+if search(list,n):
+    print("found",pos)
+else:
+    print("not found")
